191a018f66069f0b800b28fea337989755c5ee10
Korna <artem-koma@yandex.ru>
2016-07-23 22:18:44 +0700

    Colors improvment

--- src/start/App.java
+++ src/start/App.java
@@ -4,6 +4,10 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import functions.app_interface;
+import functions.app_database;
+import database.names;
+import core.app_calculation;
 
 /**
  * Created by coma on 23.07.2016.
@@ -22,7 +26,16 @@ public class App {
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Hmmmm thats the message.");
+                int day = -1, month = -1, year = -1;
+                if (e.getSource()==button1) {
+                    day = Integer.parseInt(textField1.getText());
+                    month = Integer.parseInt(textField2.getText());
+                    year = Integer.parseInt(textField3.getText());
+                }
+                String temp = functions.app_database.astro_sign(day, month);
+                temp += "\n" + core.app_calculation.calc_day_of_week(day, month, year);
+                temp += "\n" + core.app_calculation.calc_moon_phase(day, month, year);
+                JOptionPane.showMessageDialog(null, temp);
             }
         });
     }
